02e571e057add609c9ea055d4b368a5b9c2db5c8
Nova Gh <nghimire.dev@gmail.com>
2022-11-30 13:12:06 -0800

    challenge update

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -9,29 +9,67 @@ router.get("/", function (req, res, next) {
 
 /* GET Pokemon by Id. */
 router.get("/:id", function (req, res, next) {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
+  const id = req.params.id;
+  if (id == "hp") {
+    return next();
+  }
+  const pokemon = pokedex.filter((p) => p.id == id);
+  if (pokemon.length > 0) {
+    res.status(200).json(pokemon);
+  } else {
+    res.status(404).json({ message: `Error fetching pokemon by id : ${id}` });
+  }
   return;
 });
 
 /* GET Pokemon by English Name */
 router.get("/name/:name", function (req, res, next) {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
+  const engName = req.params.name;
+  const pokemon = pokedex.filter((p) => p.name.english == engName);
+  if (pokemon.length > 0) {
+    res.status(200).json(pokemon);
+  } else {
+    res.status(404).json({
+      message: `Error fetching pokemon with the english name : ${engName}`,
+    });
+  }
   return;
 });
 
 /* GET Pokemon by Type */
 router.get("/type/:type", function (req, res, next) {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
+  const type = req.params.type.toLowerCase();
+  const capitializedType = type[0].toUpperCase() + type.slice(1);
+  const pokemons = pokedex.filter((p) => p.type.includes(capitializedType));
+  if (pokemons.length > 0) {
+    res.status(200).json(pokemons);
+  } else {
+    res.status(404).json({
+      message: `Error fetching pokemon with the type : ${type}`,
+    });
+  }
   return;
 });
 
 /* GET Pokemon by HP */
 router.get("/hp", function (req, res, next) {
-  // TODO: Implement this route. See swagger docs for details, by visiting http://localhost:3000/api-docs
-  res.status(501).json({ message: "Not Implemented" });
+  const { lt, gt, lte, gte } = req.query;
+  if (req.query == {}) {
+    return res.status(404).json({ message: "No queries supplied." });
+  }
+  const pokemons = pokedex.filter((p) => {
+    const hp = p.base.HP;
+    if (hp < lt || hp > gt || hp <= lte || hp >= gte) {
+      return p;
+    }
+  });
+  if (pokemons.length > 0) {
+    res.status(200).json(pokemons);
+  } else {
+    res.status(404).json({
+      message: `Error fetching pokemon with the query ${req.query}`,
+    });
+  }
   return;
 });
 
